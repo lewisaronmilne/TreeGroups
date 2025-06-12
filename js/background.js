999948577a1e8e3293b21545b5e5b52cb1328c6c
lewisaronmilne <lewisaronmilne@gmail.com>
2025-06-12 01:34:34 +0100

    Communications between sidebar instance and background script

--- js/background.js
+++ js/background.js
@@ -1,3 +1,91 @@
+browser.runtime.onMessage.addListener(function(request)
+{
+	if (request.window_id != -1)
+		return;
+
+    switch(request.type)
+    {
+        case "populate_sidebar" : 
+            browser.tabs.query({ windowId : request.sidebar_window_id }).then(ffTabs => 
+            {
+                ffTabs.forEach(ffTab => createTab(ffTab, undefined));
+            });
+            break;
+	}	
+});
+
+/* ---------------
+-- ff listeners --
+--------------- */
+
+browser.tabs.onCreated.addListener(ffTab => 
+{ 
+    createTab(ffTab, ffTab.openerTabId);
+});
+
+browser.tabs.onRemoved.addListener((tabId, closeInfo) =>
+{ 
+    if (closeInfo.isWindowClosing) 
+        return;
+
+    removeTab(closeInfo.windowId, tabId);
+});
+
+browser.tabs.onDetached.addListener((tabId, detachInfo) => 
+{
+    removeTab(detachInfo.oldWindowId, tabId);
+});
+
+browser.tabs.onActivated.addListener(activeInfo => 
+{
+    updateTab(activeInfo.windowId, activeInfo.tabId, { "active" : true });
+
+    if (activeInfo.previousTabId) 
+        updateTab(activeInfo.windowId, activeInfo.previousTabId, { "active" : false });
+});
+
+browser.tabs.onUpdated.addListener((tabId, updateInfo, ffTab) => 
+{
+    updateTab(ffTab.windowId, tabId, updateInfo);
+});
+
+/* ------------------
+-- message senders --
+------------------ */
+
+function createTab(ffTab, parentTabId)
+{
+    browser.runtime.sendMessage(
+    { 
+        "window_id": ffTab.windowId, 
+        "type" : "create_tab",
+        "parent_tab_id" : parentTabId,
+        "tab_info" : ffTab
+    });
+}
+
+function removeTab(windowId, tabId)
+{
+    browser.runtime.sendMessage(
+    { 
+        "window_id": windowId, 
+        "type" : "remove_tab",
+        "tab_id" : tabId
+    });
+}
+
+function updateTab(windowId, tabId, updateInfo)
+{
+    browser.runtime.sendMessage(
+    { 
+        "window_id" : windowId,
+        "type" : "update_tab",
+        "tab_id" : tabId,
+        "update_info" : updateInfo
+    });
+}
+
+
 // on toolbar button clicked
 // todo. only refresh sidebar panel if opening sidebar
 browser.action.onClicked.addListener(openSidebar);
--- js/sidebar.js
+++ js/sidebar.js
@@ -1,1 +1,39 @@
-console.log("Hello, Sidebar!")
+/* ----------------
+-- sidebar init --
+---------------- */
+
+let windowId = null;
+
+browser.windows.getCurrent().then(window => 
+{
+    windowId = window.id;
+    browser.runtime.sendMessage({ "window_id" : -1, "type" : "populate_sidebar", "sidebar_window_id" : windowId });
+});
+
+/* -------------------
+-- receive requests --
+------------------- */
+
+browser.runtime.onMessage.addListener(sidebarReq)
+function sidebarReq(request)
+{
+	if (request.window_id != windowId)
+		return;
+
+    console.log(request)
+
+	// switch(request.type)
+	// {
+    //     case "create_tab" :
+    //         createTab(request.tab_info, getTab(request.parent_tab_id));
+	// 	    break;
+
+    //     case "remove_tab" :
+    //         getTab(request.tab_id).remove();
+    //         break;
+
+    //     case "update_tab" :
+    //         getTab(request.tab_id).update(request.update_info);
+    //         break;
+    // }
+}
